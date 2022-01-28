59d3c22554d340499d8bedcaba667b5fb49d88b6
5hadown1 <shadownight@inbox.ru>
2022-01-28 19:19:05 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { Message } from './components/Message';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <Message text="Props message"/>
       </header>
     </div>
   );
---
+++ src/components/Message/index.js
@@ -0,0 +1,3 @@
+import "./styles.css";
+
+export const Message = ({ text }) => <h3 className="text">{ text }</h3>;
