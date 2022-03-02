15a57179e480a1e467aa918f34344637a0394dd6
5hadown1 <shadownight@inbox.ru>
2022-03-02 22:32:55 +0700

    Add redux-persist

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,7 @@
 import { combineReducers, createStore, compose, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from 'redux-persist/lib/storage';
 import { profileReducer } from "./profile/reducer";
 import { chatsReducer } from "./chats/reducer";
 import { messagesReducer } from "./messages/reducer";
@@ -7,12 +9,21 @@ import { messagesReducer } from "./messages/reducer";
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const rootReducer = combineReducers({
-    profile: profileReducer,
-    chats: chatsReducer,
-    messages: messagesReducer
+	profile: profileReducer,
+	chats: chatsReducer,
+	messages: messagesReducer
 });
 
+const persistConfig = {
+	key: 'root',
+	storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = createStore(
-    rootReducer,
-    composeEnhancers(applyMiddleware(thunk))
+	persistedReducer,
+	composeEnhancers(applyMiddleware(thunk))
 );
+	
+export const persistor = persistStore(store);
